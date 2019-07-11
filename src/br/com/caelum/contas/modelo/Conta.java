71bd9fa067bbc0e04311ed45a1279aff415a4a5c
Matheus Barbosa <matheusb565@gmail.com>
2019-07-11 14:11:38 -0300

    Sobrescrito os métodos equals e toString da classe Conta

--- src/br/com/caelum/contas/modelo/Conta.java
+++ src/br/com/caelum/contas/modelo/Conta.java
@@ -92,4 +92,21 @@ public abstract class Conta {
 		this.saca(valor);
 		conta.deposita(valor);
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		
+		Conta outraConta = (Conta) obj;
+		
+		return this.numero == outraConta.numero && this.agencia.equals(outraConta.agencia);
+	}
+	
+	@Override
+	public String toString() {
+		return "[titular=" + titular + ", numero=" + numero 
+				+ ", agencia=" + agencia + "]";
+	}
 }
