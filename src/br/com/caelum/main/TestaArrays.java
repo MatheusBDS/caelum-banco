eaece29a45e004b9fe472762ff22344d591bda63
Matheus Barbosa <matheusb565@gmail.com>
2019-07-12 18:51:47 -0300

    Criado a classe TestaSplit e atualizando a TestaArrays

--- src/br/com/caelum/main/TestaArrays.java
+++ src/br/com/caelum/main/TestaArrays.java
@@ -7,15 +7,21 @@ public class TestaArrays {
 
 	public static void main(String[] args) {
 		Conta[] contas = new Conta[10];
+		double media = 0;
 		
 		for (int i = 0; i < contas.length; i++) {
 			Conta conta = new ContaCorrente();
 			conta.deposita(i * 100.0);
-			System.out.println(contas.toString());
+			System.out.println(conta.getSaldo());
+			
+			media += conta.getSaldo();
+			
 		}
 		
+		media /= contas.length;
 		
-		
+		System.out.println("Média dos Saldos: " + media);
+				
 	}
 
 }
---
+++ src/br/com/caelum/main/TestaSplit.java
@@ -0,0 +1,12 @@
+package br.com.caelum.main;
+
+public class TestaSplit {
+
+	public static void main(String[] args) {
+		String frase = "Uma mensagem qualquer";
+		String[] palavras = frase.split("");
+		
+		System.out.println(palavras);
+	}
+
+}
