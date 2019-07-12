b5ccff56cdc208497e064796f75acf1f19ea372e
Matheus Barbosa <matheusb565@gmail.com>
2019-07-12 14:06:01 -0300

    Criado a classe de testes com array de contas

---
+++ src/br/com/caelum/main/TestaArrays.java
@@ -0,0 +1,21 @@
+package br.com.caelum.main;
+
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
+
+public class TestaArrays {
+
+	public static void main(String[] args) {
+		Conta[] contas = new Conta[10];
+		
+		for (int i = 0; i < contas.length; i++) {
+			Conta conta = new ContaCorrente();
+			conta.deposita(i * 100.0);
+			System.out.println(contas.toString());
+		}
+		
+		
+		
+	}
+
+}
