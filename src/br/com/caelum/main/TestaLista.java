05ec3b014b685fac8f19d18496ab20f2809f52ee
Matheus Barbosa <matheusb565@gmail.com>
2019-08-06 14:20:05 -0300

    Criação da Classe TestaLista

---
+++ src/br/com/caelum/main/TestaLista.java
@@ -0,0 +1,16 @@
+package br.com.caelum.main;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.caelum.contas.modelo.Conta;
+
+public class TestaLista {
+	public static void main(String[] args) {
+
+		List<Conta> contas = new LinkedList<Conta>();
+
+		
+		
+	}
+}
